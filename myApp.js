fee434f464bd2009d3227a10a703b09e226908f5
Anshika Gupta <ansrkghamirpur@gmail.com>
2025-10-03 15:15:23 +0530

    Route to serve index.html

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Express'); 
-})
+const path = require('path'); 
 
+// Route to serve index.html
+app.get('/', function(req, res) {
+  const absolutePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(absolutePath);
+});
+
+module.exports = app;
 
 
 
